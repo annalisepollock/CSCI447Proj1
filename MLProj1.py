29b04a68a472d6593e29d34ba8421406b9b06922
Elizabeth Pauley <elizpauley@gmail.com>
2024-09-09 12:52:49 -0600

    Initial data cleaning

--- MLProj1.py
+++ MLProj1.py
@@ -1,24 +1,57 @@
+import math
 import pandas as pd
 import Learner
+from ucimlrepo import fetch_ucirepo
 
 def main():
-    dataFile = open("/Users/annalise/Downloads/breast-cancer-wisconsin.data", "r")
-    data = pd.DataFrame(dataFile)[0].str.split(',', expand=True).replace('\n', '', regex=True)
-    data = data.head(20)
-    print(data)
-    #data = cleanData(data, False)
-    learner = Learner.Learner(data, 10)
+    # fetch datasets
+    breastCancerData =  fetch_ucirepo(id=15)
+    breastCancerDataFrame = pd.DataFrame(breastCancerData.data.original)
+
+    # BREAST CANCER DATASET CLEANING
+    # curated data cleaning: breast cancer data
+    breastCancerNoId = breastCancerDataFrame.drop(columns=['Sample_code_number']) # remove ID column
+
+    breastCancerClean = cleanData(breastCancerData, breastCancerNoNull, False)
+    breastCancerClean['Bare_nuclei'] = breastCancerClean['Bare_nuclei'].astype(int)
+
+    # END BREAST CANCER DATASET CLEANING
+
+    #learner = Learner.Learner(data, 10)
     print("Done")
 
+def cleanData(dataOriginal, dataSet, noise):
+    dataVariables = pd.DataFrame(dataOriginal.variables)
+    print(dataVariables.to_string())
+    #dataMetadata = dataSet.metadata
 
-def cleanData(dataSet, noise):
     if(noise):
         addNoise(dataSet)
-    #clean data
-    return dataSet
+
+    # Remove any rows where all values are null
+    dataRemovedNullRows = dataSet.dropna(how = 'all')
+
+    # Columns must have 90% of their values for rows to remain in dataset
+    dataRemovedNullCols = dataRemovedNullRows.dropna(axis=1, thresh = math.floor(0.90*dataSet.shape[0]))
+
+    # Iterate through columns; if numerical, fillna with mean
+    # if categorical/binary, use forward fill/backfill
+    # round values to nearest int so that na's can be filled with this value regardless if continuous or discrete
+    dataSetNoNull = dataRemovedNullCols.fillna(round(dataRemovedNullCols.mean()))
+
+    #print(dataSetNoNull.to_string())
+    # Continuous attributes - discretize
+
+
+    return dataSetNoNull
 
 def addNoise(dataSet):
-    #add noise to the data set
+    # add noise to the data set
     return dataSet
+
+def crossValidation(cleanDataset):
+        # randomly split data into 10 parts
+        # rotate each part to be used as testing 1x
+        # call learn, classify, etc.
 
 main()
