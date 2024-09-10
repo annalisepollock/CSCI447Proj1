b57466625b7f3cd9447e5ed85b8bdc20f03f417f
Elizabeth Pauley <elizpauley@gmail.com>
2024-09-10 11:22:48 -0600

    binning, encoding

--- MLProj1.py
+++ MLProj1.py
@@ -1,12 +1,24 @@
 import math
 import pandas as pd
+import numpy as np
 import Learner
 from ucimlrepo import fetch_ucirepo
+from sklearn.preprocessing import OneHotEncoder
 
 def main():
-    # fetch datasets
+    # FETCH DATASETS
     breastCancerData =  fetch_ucirepo(id=15)
     breastCancerDataFrame = pd.DataFrame(breastCancerData.data.original)
+    breastCancerColumnTypes = dict(zip(breastCancerData.variables['name'], breastCancerData.variables['type']))
+
+    glassData = fetch_ucirepo(id=42)
+    glassDataFrame = pd.DataFrame(glassData.data.original)
+    glassColumnTypes = dict(zip(glassData.variables['name'], glassData.variables['type']))
+
+    irisData = fetch_ucirepo(id=53)
+    irisDataFrame = pd.DataFrame(irisData.data.original)
+    irisColumnTypes = dict(zip(irisData.variables['name'], irisData.variables['type']))
+    # END FETCH DATASETS
 
     # BREAST CANCER DATASET CLEANING
     # curated data cleaning: breast cancer data
@@ -16,12 +28,42 @@ def main():
     breastCancerClean['Bare_nuclei'] = breastCancerClean['Bare_nuclei'].astype(int)
     # END BREAST CANCER DATASET CLEANING
 
+    # GLASS DATASET CLEANING/BINNING
+    glassDataFrame = glassDataFrame.drop(columns=['Id_number'])
+
+    # binning, if continuous/categorical
+    for columnName in glassDataFrame.columns:
+        # TO-DO: ignore Class column
+        # if continuous, make categorical (5 categories total)
+        if glassColumnTypes[columnName] == 'Continuous':
+            # split dataset into 5 equal-width bins
+            binVals = np.linspace(glassDataFrame[columnName].min(), glassDataFrame[columnName].max(), 6)
+            binLabels = ['A', 'B', 'C', 'D', 'E']
+
+            # assign column vals to a bin
+            glassDataFrame[columnName] = pd.cut(glassDataFrame[columnName], bins = binVals, labels = binLabels, include_lowest = True)
+            # set 'type' to Categorical
+            glassColumnTypes[columnName] = 'Categorical'
+
+        if glassColumnTypes[columnName] == 'Categorical':
+            # one-hot encoding
+            encoder = OneHotEncoder()
+
+            # encode all columns except for the class column
+            glassColumnsToEncode = glassDataFrame.iloc[:, :-1]
+
+            glassDataEncoded = pd.DataFrame(encoder.fit_transform(glassColumnsToEncode).toarray())
+            glassClean = pd.concat([glassDataEncoded, glassDataFrame.iloc[:, -1]], axis=1)
+    # END GLASS DATASET CLEANING/BINNING
+
+    # CROSS-VALIDATION, TRAINING + TESTING
     breastCancerFolds = crossValidation(breastCancerClean)
+    glassFolds = crossValidation(glassClean)
 
     #learner = Learner.Learner(data, 10)
     print("Done")
 
-def cleanData(dataOriginal, dataSet, noise):
+def cleanData(dataOriginal, dataSet, noise, classColumnName):
     dataVariables = pd.DataFrame(dataOriginal.variables)
 
     if(noise):
@@ -30,8 +72,8 @@ def cleanData(dataOriginal, dataSet, noise):
     # Remove any rows where all values are null
     dataRemovedNullRows = dataSet.dropna(how = 'all')
 
-    # Columns must have 90% of their values for rows to remain in dataset
-    dataRemovedNullCols = dataRemovedNullRows.dropna(axis=1, thresh = math.floor(0.90*dataSet.shape[0]))
+    # Columns must have 70% of their values for rows to remain in dataset
+    dataRemovedNullCols = dataRemovedNullRows.dropna(axis=1, thresh = math.floor(0.70*dataSet.shape[0]))
 
     # Iterate through columns; if numerical, fillna with mean
     # if categorical/binary, use forward fill/backfill
@@ -40,7 +82,6 @@ def cleanData(dataOriginal, dataSet, noise):
 
     #print(dataSetNoNull.to_string())
     # Continuous attributes - discretize
-
 
     return dataSetNoNull
 
@@ -57,13 +98,13 @@ def crossValidation(cleanDataset):
     for i in range(9):
         # randomly select 1/10 of the dataset, put it in the array
         chunk = tempDataset.sample(n=numRows)
-        print("chunk sample size for " + str(i) + ": " + str(chunk.shape[0]))
+        #print("chunk sample size for " + str(i) + ": " + str(chunk.shape[0]))
         dataChunks[i] = chunk
 
         # rest of dataset without selected chunk
         tempDataset = tempDataset.drop(chunk.index)
 
-    print("size of remaining data: " + str(tempDataset.shape[0]))
+    #print("size of remaining data: " + str(tempDataset.shape[0]))
     # the last chunk might be slightly different size if dataset size is not divisible by 10
     dataChunks[9] = tempDataset
 
